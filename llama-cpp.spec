Man2Dev/llama-cpp

commit 86d251d01791db5b96eccd8d7784814873ef3927

Update to b4094
match llama-cpp-python-0.3.2:
https://github.com/abetlen/llama-cpp-python/tree/v0.3.2/vendor

Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

llama-cpp.spec
@@ -37,7 +37,7 @@ Name:           llama-cpp
 # This is the main license
 
 License:        MIT AND Apache-2.0 AND LicenseRef-Fedora-Public-Domain
-Version:        b3837
+Version:        b4094
 Release:        %autorelease
 
 URL:            https://github.com/ggerganov/llama.cpp
@@ -237,6 +237,7 @@ rm %{buildroot}%{_bindir}/convert*.py
 %license LICENSE
 %{_libdir}/libllama.so.%{version}
 %{_libdir}/libggml.so.%{version}
+%{_libdir}/libggml-base.so.%{version}
 
 %files devel
 %dir %{_libdir}/cmake/llama
@@ -246,6 +247,8 @@ rm %{buildroot}%{_bindir}/convert*.py
 %{_includedir}/llama.h
 %{_libdir}/libllama.so
 %{_libdir}/libggml.so
+%{_libdir}/libggml-base.so
+%{_libdir}/libggml-cpu.so
 %{_libdir}/cmake/llama/*.cmake
 %{_exec_prefix}/lib/pkgconfig/llama.pc
 

.gitignore
@@ -1,3 +1,4 @@
 /llama.cpp-b2879.tar.gz
 /llama.cpp-b3667.tar.gz
 /llama.cpp-b3837.tar.gz
+/llama.cpp-b4094.tar.gz

sources
@@ -1,1 +1,1 @@
-SHA512 (llama.cpp-b3837.tar.gz) = ac0c5ed0793f3c770031175a07132542beb2d45c31830c6ed510c1a6d814e6f4a2bfba484b9f6f1471c05ceaf54a629da3b896b26a0381b594a6b49c964afd41
+SHA512 (llama.cpp-b4094.tar.gz) = c78fc3bf78d231ae54d32ea3e2281099b5b7064f28ba259cf1e45849a1d9a97f2a2d6197f6e9a73ddfe7f8b1585c23faa9d3a322578564d79dfd5d2a9ceec550

commit 329939bab49a33a74adf3aa7d7de636734d833a7

Enable the GPU accelerated HIP code
The CMake build option to use HIP was renamed from LLAMA_HIPBLAS to
GGML_HIPBLAS [1] in llama.cpp b3240, and then from GGML_HIPBLAS to
GGML_HIP [2] in llama.cpp b4080.

Even though commit c5d0cb291f229043 started using the ROCm toolchain on
x86_64, until now it was still only building the CPU code and not the
GPU accelerated HIP code.

Fallout from 86d251d01791db5b96eccd8d7784814873ef3927 and
c5d0cb291f229043972dbd3c6409765a9c6937cf

[1] llama.cpp commit f3f65429c44bb195
    https://github.com/ggerganov/llama.cpp/commit/f3f65429c44bb195
    https://github.com/ggerganov/llama.cpp/pull/8006

[2] llama.cpp commit ae8de6d50a09d495
    https://github.com/ggerganov/llama.cpp/commit/ae8de6d50a09d495
    https://github.com/ggerganov/llama.cpp/pull/10256

https://src.fedoraproject.org/rpms/llama-cpp/pull-request/11

llama-cpp.spec
@@ -190,7 +190,7 @@ module load rocm/default
     -DGGML_FMA=OFF \
     -DGGML_F16C=OFF \
 %if %{with rocm}
-    -DLLAMA_HIPBLAS=%{build_hip} \
+    -DGGML_HIP=%{build_hip} \
     -DAMDGPU_TARGETS=${ROCM_GPUS} \
 %endif
     -DLLAMA_BUILD_EXAMPLES=%{build_examples} \
@@ -247,6 +247,9 @@ rm %{buildroot}%{_bindir}/convert*.py
 %{_libdir}/libggml.so
 %{_libdir}/libggml-base.so
 %{_libdir}/libggml-cpu.so
+%if %{with rocm}
+%{_libdir}/libggml-hip.so
+%endif
 %{_libdir}/cmake/llama/*.cmake
 %{_exec_prefix}/lib/pkgconfig/llama.pc
 