Man2Dev/llama-cpp

commit 14bc2eade72946527ecfc44e4ed5b6eaef84bd23

Update to b3667
Signed-off-by: Tom Rix <Tom.Rix@amd.com>

llama-cpp.spec
@@ -1,10 +1,21 @@
 # For the extra python package gguf that comes with llama-cpp
 %global pypi_name gguf
-%global pypi_version 0.9.0
+%global pypi_version 0.10.0
 
 # Some optional subpackages
 %bcond_with examples
+%if %{with examples}
+%global build_examples ON
+%else
+%global build_examples OFF
+%endif
+
 %bcond_with test
+%if %{with test}
+%global build_test ON
+%else
+%global build_test OFF
+%endif
 
 Summary:        Port of Facebook's LLaMA model in C/C++
 Name:           llama-cpp
@@ -24,7 +35,7 @@ Name:           llama-cpp
 # This is the main license
 
 License:        MIT AND Apache-2.0 AND LicenseRef-Fedora-Public-Domain
-Version:        b3184
+Version:        b3667
 Release:        %autorelease
 
 URL:            https://github.com/ggerganov/llama.cpp
@@ -114,17 +125,13 @@ Requires:       %{name}%{?_isa} = %{version}-%{release}
 %autosetup -p1 -n llama.cpp-%{version}
 
 # verson the *.so
-sed -i -e 's/POSITION_INDEPENDENT_CODE ON/POSITION_INDEPENDENT_CODE ON SOVERSION %{version}/' CMakeLists.txt
+sed -i -e 's/POSITION_INDEPENDENT_CODE ON/POSITION_INDEPENDENT_CODE ON SOVERSION %{version}/' src/CMakeLists.txt
+sed -i -e 's/POSITION_INDEPENDENT_CODE ON/POSITION_INDEPENDENT_CODE ON SOVERSION %{version}/' ggml/src/CMakeLists.txt
 
 # no android needed
 rm -rf exmples/llma.android
 # git cruft
 find . -name '.gitignore' -exec rm -rf {} \;
-# scripts need to be executable
-chmod 755 examples/finetune/convert-finetune-checkpoint-to-gguf.py
-chmod 755 examples/finetune/finetune.sh
-chmod 755 examples/train-text-from-scratch/convert-train-checkpoint-to-gguf.py
-
 
 %build
 %if %{with examples}
@@ -135,6 +142,7 @@ cd -
 
 %cmake \
     -DCMAKE_INSTALL_LIBDIR=%{_lib} \
+    -DCMAKE_SKIP_RPATH=ON \
     -DLLAMA_AVX=OFF \
     -DLLAMA_AVX2=OFF \
     -DLLAMA_AVX512=OFF \
@@ -142,16 +150,8 @@ cd -
     -DLLAMA_AVX512_VNNI=OFF \
     -DLLAMA_FMA=OFF \
     -DLLAMA_F16C=OFF \
-%if %{with examples}
-    -DLLAMA_BUILD_EXAMPLES=ON \
-%else
-    -DLLAMA_BUILD_EXAMPLES=OFF \
-%endif
-%if %{with test}
-    -DLLAMA_BUILD_TESTS=ON
-%else
-    -DLLAMA_BUILD_TESTS=OFF
-%endif
+    -DLLAMA_BUILD_EXAMPLES=%{build_examples} \
+    -DLLAMA_BUILD_TESTS=%{build_test}
     
 %cmake_build
 
@@ -184,106 +184,35 @@ rm %{buildroot}%{_bindir}/convert*.py
 %files
 %license LICENSE
 %{_libdir}/libllama.so.%{version}
+%{_libdir}/libggml.so.%{version}
 
 %files devel
-%dir %{_libdir}/cmake/Llama
+%dir %{_libdir}/cmake/llama
 %doc README.md
 %{_includedir}/ggml.h
-%{_includedir}/ggml-alloc.h
-%{_includedir}/ggml-backend.h
+%{_includedir}/ggml-*.h
 %{_includedir}/llama.h
 %{_libdir}/libllama.so
-%{_libdir}/cmake/Llama/*.cmake
+%{_libdir}/libggml.so
+%{_libdir}/cmake/llama/*.cmake
 %{_exec_prefix}/lib/pkgconfig/llama.pc
 
 %if %{with test}
 %files test
-%{_bindir}/test-autorelease
-%{_bindir}/test-backend-ops
-%{_bindir}/test-chat-template
-%{_bindir}/test-grad0
-%{_bindir}/test-grammar-parser
-%{_bindir}/test-llama-grammar
-%{_bindir}/test-model-load-cancel
-%{_bindir}/test-quantize-fns
-%{_bindir}/test-quantize-perf
-%{_bindir}/test-rope
-%{_bindir}/test-sampling
-%{_bindir}/test-tokenizer-0-falcon
-%{_bindir}/test-tokenizer-0-llama
-%{_bindir}/test-tokenizer-1-bpe
-%{_bindir}/test-tokenizer-1-llama
+%{_bindir}/test-*
 %endif
 
 %if %{with examples}
 %files examples
-%{_bindir}/baby-llama
-%{_bindir}/batched
-%{_bindir}/batched-bench
-%{_bindir}/beam-search
-%{_bindir}/benchmark
-%{_bindir}/convert-lora-to-ggml.py
-%{_bindir}/convert.py
-%{_bindir}/convert-llama2c-to-ggml
-%{_bindir}/embedding
-%{_bindir}/export-lora
-%{_bindir}/finetune
-%{_bindir}/gguf
-%{_bindir}/gguf-convert-endian
-%{_bindir}/gguf-dump
-%{_bindir}/gguf-set-metadata
-%{_bindir}/gritlm
-%{_bindir}/imatrix
-%{_bindir}/infill
-%{_bindir}/llama-bench
-%{_bindir}/llava-cli
-%{_bindir}/lookahead
-%{_bindir}/lookup
-%{_bindir}/main
-%{_bindir}/parallel
-%{_bindir}/passkey
-%{_bindir}/perplexity
-%{_bindir}/quantize
-%{_bindir}/quantize-stats
-%{_bindir}/save-load-state
-%{_bindir}/server
-%{_bindir}/simple
-%{_bindir}/speculative
-%{_bindir}/tokenize
-%{_bindir}/train-text-from-scratch
-%{_libdir}/libllava_shared.so
+%{_bindir}/convert_hf_to_gguf.py
+%{_bindir}/gguf-*
+%{_bindir}/llama-*
 %{_datarootdir}/%{name}/
+%{_libdir}/libllava_shared.so
 %{python3_sitelib}/%{pypi_name}
-%{python3_sitelib}/%{pypi_name}-%{pypi_version}.dist-info
+%{python3_sitelib}/%{pypi_name}*.dist-info
 %{python3_sitelib}/scripts
 %endif
 
 %changelog
-* Sat Jun 22 2024 Mohammadreza Hendiani <man2dev@fedoraproject.org> - b3184-1
-- Update to b3184 (required by llama-cpp-python-0.2.79)
-- added .pc file
-
-* Sat May 18 2024 Mohammadreza Hendiani <man2dev@fedoraproject.org> -  b2879-2
-- Removed useless cmake flags
-- added dependancies based .github/workflows/server.yml (for ubuntu) in upstream
-
-* Fri May 17 2024 Mohammadreza Hendiani <man2dev@fedoraproject.org> -  b2879-1
-- Update to  b2879 (required by llama-cpp-python-0.2.75)
-- fixed build bugs
-
-* Sun May 12 2024 Mohammadreza Hendiani <man2dev@fedoraproject.org> - b2861-1
-- Update to b2861 (required by llama-cpp-python-0.2.74)
-- Added ccache as BuildRequires
-- Added numctl as Weak dependency
-
-* Thu Apr 11 2024 Tomas Tomecek <ttomecek@redhat.com> - b2619-1
-- Update to b2619 (required by llama-cpp-python-0.2.60)
-
-* Sat Mar 23 2024 Tom Rix <trix@redhat.com> - b2417-2
-- Fix test subpackage
-
-* Thu Mar 14 2024 Tom Rix <trix@redhat.com> - b2417-1
-- Update to b2417
-
-* Sat Dec 23 2023 Tom Rix <trix@redhat.com> - b1695-1
-- Initial package
+%autochangelog

.gitignore
@@ -1,1 +1,2 @@
 /llama.cpp-b2879.tar.gz
+/llama.cpp-b3667.tar.gz

sources
@@ -1,1 +1,2 @@
 SHA512 (llama.cpp-b3184.tar.gz) = 6ffb3d55a1a0258673a4eee77b4a20f4b9ec438eabe9973399080d518e65bfa358119aea809079ef7f4da6ada203e484a9fdfd1c9f47e58b0179454fd0fcfb2e
+SHA512 (llama.cpp-b3667.tar.gz) = c9e50f8add5ca6a16d5c26e8d74d75c476f297d9a72e454e7938cea04d2302a7810f822ced846976d0dbc13fea357d5890317b6653450a757bf61e38e6790b41

commit e23bc3abb2c67d7ac78021977a41a4b6c04864ab

Fix the build options to use AVX, FMA and F16C instructions
The CMake build options to use AVX, FMA and F16C instructions were
renamed from LLAMA_* to GGML_* [1] in llama.cpp b3240.

Fallout from 14bc2eade72946527ecfc44e4ed5b6eaef84bd23

[1] llama.cpp commit f3f65429c44bb195
    https://github.com/ggerganov/llama.cpp/commit/f3f65429c44bb195
    https://github.com/ggerganov/llama.cpp/pull/8006

https://src.fedoraproject.org/rpms/llama-cpp/pull-request/11

llama-cpp.spec
@@ -182,13 +182,13 @@ module load rocm/default
 %cmake \
     -DCMAKE_INSTALL_LIBDIR=%{_lib} \
     -DCMAKE_SKIP_RPATH=ON \
-    -DLLAMA_AVX=OFF \
-    -DLLAMA_AVX2=OFF \
-    -DLLAMA_AVX512=OFF \
-    -DLLAMA_AVX512_VBMI=OFF \
-    -DLLAMA_AVX512_VNNI=OFF \
-    -DLLAMA_FMA=OFF \
-    -DLLAMA_F16C=OFF \
+    -DGGML_AVX=OFF \
+    -DGGML_AVX2=OFF \
+    -DGGML_AVX512=OFF \
+    -DGGML_AVX512_VBMI=OFF \
+    -DGGML_AVX512_VNNI=OFF \
+    -DGGML_FMA=OFF \
+    -DGGML_F16C=OFF \
 %if %{with rocm}
     -DLLAMA_HIPBLAS=%{build_hip} \
     -DAMDGPU_TARGETS=${ROCM_GPUS} \